2be997473e3d9a0f918c409528ad4b97adf3b52b
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2020-09-11 21:25:40 +0300

    Implement normal mapping for classic Mandelbrot

--- classic.py
+++ classic.py
@@ -3,6 +3,9 @@ from _routines import ffi, lib
 from threading import Thread, Lock
 
 
+EPSILON = 1e-12
+
+
 def escape_time(z, c, exponent, twisors, escaped):
     for i, t in enumerate(twisors):
         escaped[np.logical_and(escaped < 0, abs(z) >= 128)] = i
@@ -37,13 +40,80 @@ def mandelbrot(width, height, center_x, center_y, zoom, rotation, exponent, twis
         if julia:
             escape_time(z, z*0 + julia_c, exponent, twisors, outside)
         else:
-            escape_time(z, z*1, exponent, twisors, outside)
+            escape_time(z, z+0, exponent, twisors, outside)
 
         w = outside > 0
         outside[w] -= np.log(np.log(abs(z[w]))) / np.log(exponent)
-        outside[~w] = len(twisors) - np.log(np.log(128)*2) / np.log(exponent)
+        outside[~w] = len(twisors) - np.log(np.log(128)*2) / np.log(exponent)  # TODO: Fix
 
         subpixel_image = color_map(outside)
+
+        lock.acquire()
+        result += subpixel_image
+        lock.release()
+
+    ts = []
+    offsets = np.arange(anti_aliasing) / anti_aliasing
+    for i in offsets:
+        for j in offsets:
+            ts.append(Thread(target=accumulate_subpixels, args=(i, j)))
+            ts[-1].start()
+    for t in ts:
+        t.join()
+
+    result /= anti_aliasing**2
+
+    return result
+
+
+def escape_time_dz(z, dz, c, dc, exponent, escaped, max_iter):
+    for i in range(max_iter):
+        escaped[np.logical_and(escaped < 0, abs(z) >= 128)] = i
+        s = escaped < 0
+        dz[s] = dz[s]*exponent*z[s]**(exponent-1) + dc[s]
+        z[s] = z[s]**exponent + c[s]
+
+
+def mandelbrot_dx(width, height, center_x, center_y, zoom, rotation, exponent, max_iter, color_map, anti_aliasing=2, julia_c=1j, julia=False):
+    lock = Lock()
+
+    num_color_channels = 3
+    result = np.zeros((num_color_channels, height, width))
+
+    c, s = np.cos(rotation), np.sin(rotation)
+    zoom = 2**-zoom / height
+    def accumulate_subpixels(offset_x, offset_y):
+        nonlocal result
+        x = np.arange(width, dtype='float64') + offset_x
+        y = np.arange(height, dtype='float64') + offset_y
+
+        x, y = np.meshgrid(x, y)
+
+        x = (2 * x - width) * zoom
+        y = (2 * y - height) * zoom
+
+        x, y = x*c + y*s, y*c - x*s
+        x += center_x
+        y += center_y
+
+        z = x + 1j*y
+        dz = 1 + 0j*z
+        outside = x*0 - 1
+        if julia:
+            escape_time_dz(z, dz, z*0 + julia_c, z*0, exponent, outside, max_iter)
+        else:
+            escape_time_dz(z, dz, z+0, 1 + z*0, exponent, outside, max_iter)
+
+        r = abs(z)
+        u = dz/(z + (r < EPSILON))
+        ru = abs(u)
+        u /= ru + (ru < EPSILON)
+
+        w = outside > 0
+        outside[w] = np.log(np.log(r[w])) / np.log(exponent) - outside[w] + max_iter - 1 - np.log(np.log(128)) / np.log(exponent)
+        outside[~w] = 0
+
+        subpixel_image = color_map(outside, np.real(u), np.imag(u))
 
         lock.acquire()
         result += subpixel_image
--- render_image.py
+++ render_image.py
@@ -19,17 +19,26 @@ if __name__ == '__main__':
     # Instagram
     width, height = 108*scale, 108*scale
 
-    anti_aliasing = 3
-    num_total_samples = 2**34
+    anti_aliasing = 6
+    # num_total_samples = 2**34
 
-    max_iter = 7
-    min_iter = max_iter - 1
+    max_iter = 16
+    # min_iter = max_iter - 1
 
     zoom = -1
-    rotation = pi*0.5
-    x, y = 0.5, 0
+    rotation = 0
+    x, y = -1, 0
+    light_height = 1.5
+    light_angle = 0.4
+    light_x = cos(light_angle)
+    light_y = sin(light_angle)
 
-    # image = mandelbrot(width, height, x, y, zoom, 3.5, max_iter, color_map=color_map, anti_aliasing=anti_aliasing, inside_cutoff=1024, clip_outside=True)
+    def color_map(outside, dx, dy):
+        l = (light_x*dx + light_y*dy + light_height) / (1 + light_height)
+        return array([outside + 7*(outside==0), 0.5*outside + 2*(outside==0), 0.2*outside + 3*(outside==0)])*l*0.08 + (outside==0)*0.05
+
+    image = classic.mandelbrot_dx(width, height, x, y, zoom, rotation, 2.0, max_iter, color_map=color_map, anti_aliasing=anti_aliasing)
+
     # image = mandelbrot_generic(
     #     width, height, x, y, zoom, 3*pi/4,
     #     4, 2,
@@ -37,22 +46,22 @@ if __name__ == '__main__':
     #     julia_c=(randn() + randn()*1j), julia=False
     # )
 
-    image = []
+    # image = []
 
-    for channel in range(3):
-        def generator(num_samples):
-            return 4*rand(num_samples)-2 + 1j*(4*rand(num_samples)-2)
+    # for channel in range(3):
+    #     def generator(num_samples):
+    #         return 4*rand(num_samples)-2 + 1j*(4*rand(num_samples)-2)
 
-        exposure = buddhabrot(anti_aliasing*width, anti_aliasing*height, x, y, zoom, rotation, 5, 2, max_iter+channel, min_iter+channel, generator, num_total_samples, 16)
+    #     exposure = buddhabrot(anti_aliasing*width, anti_aliasing*height, x, y, zoom, rotation, 3, 2, max_iter+channel, min_iter+channel, generator, num_total_samples, 16)
 
-        exposure = tanh((exposure * ((channel+1)*15*width*height / num_total_samples))**0.8)
+    #     exposure = tanh((exposure * ((channel+1)*15*width*height / num_total_samples))**0.8)
 
-        result = exposure[::anti_aliasing, ::anti_aliasing]*0
-        for i in range(anti_aliasing):
-            for j in range(anti_aliasing):
-                result += exposure[i::anti_aliasing, j::anti_aliasing]
-        result /= anti_aliasing**2
-        image.append(result)
-    image = array(image)
+    #     result = exposure[::anti_aliasing, ::anti_aliasing]*0
+    #     for i in range(anti_aliasing):
+    #         for j in range(anti_aliasing):
+    #             result += exposure[i::anti_aliasing, j::anti_aliasing]
+    #     result /= anti_aliasing**2
+    #     image.append(result)
+    # image = array(image)
 
     imsave("/tmp/out.png", make_picture_frame(image))
